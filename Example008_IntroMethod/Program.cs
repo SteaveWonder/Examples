c7f8a6aa175dbddb5f03abf89f9c7c056835e5c4
Stepan <soulsteff@gmail.com>
2023-03-26 17:10:11 +0300

    intro

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿int Max(int num1, int num2, int num3)
+{
+    int result = num1;
+    if(num2 > result) result = num2;
+    if(num3 > result) result = num3;
+    return result;
+} 
+int a1 = 45872;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 3333;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+
+// int max1 = Max (a1, b1, c1);
+// int max2 = Max (a2, b2, c2);
+// int max3 = Max (a3, b3, c3);
+// int max = Max (max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+
+// if (a3 > max) max = a3;
+// if (b3 > max) max = b3;
+// if (c3 > max) max = c3;
